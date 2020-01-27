4cd6566d04717694b0114d96a9a040f9ff56c2d6
Karen <karenbrady92@hotmail.co.uk>
2020-01-27 14:18:04 +0000

    GET added

---
+++ database.sql
@@ -0,0 +1,89 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: tr-database.cgp6e8kzb9z4.eu-west-1.rds.amazonaws.com    Database: tasks
+-- ------------------------------------------------------
+-- Server version	5.7.26-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED='';
+
+--
+-- Table structure for table `Tasks`
+--
+
+DROP TABLE IF EXISTS `Tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Tasks` (
+  `taskId` int(11) NOT NULL AUTO_INCREMENT,
+  `task_name` varchar(255) NOT NULL,
+  `complete` tinyint(1) DEFAULT NULL,
+  `due_date` date DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`taskId`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `Tasks_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `Users` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Tasks`
+--
+
+LOCK TABLES `Tasks` WRITE;
+/*!40000 ALTER TABLE `Tasks` DISABLE KEYS */;
+INSERT INTO `Tasks` VALUES (1,'Learn HTML',1,'2019-07-23',1),(2,'Learn CSS',1,'2019-08-13',1),(3,'Learn Javascript',1,'2019-09-04',1),(4,'Learn React',0,'2019-10-12',1),(5,'Walk dogs',1,'2019-07-14',2),(6,'Clean car',1,'2019-08-26',2),(7,'Book holiday',0,'2019-12-16',2),(8,'Buy new TV',0,'2020-01-08',2),(9,'Clean Oven',1,'2019-11-08',3),(10,'Buy Christmas Presents',1,'2019-12-17',3),(11,'Buy Valentines Presents',1,'2020-02-14',3),(12,'Study for Exam',0,'2020-03-04',3);
+/*!40000 ALTER TABLE `Tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'Sheila'),(2,'Melvin'),(3,'Jim');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-27  0:07:58
---
+++ handler.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const serverlessHttp = require("serverless-http");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// GET
+
+app.get("/tasks", function (request, response) {
+
+	console.log(request);
+
+	response.status(200).json({
+
+    tasks: [
+      {
+        id: 1,
+        taskName: "Learn HTML",
+        complete: true,
+        dueDate: "2019-11-28",
+      },
+      {
+        id: 2,
+        taskName: "Learn CSS",
+        complete: true,
+        dueDate: "2019-12-02",
+      },
+      {
+        id: 3,
+        taskName: "Learn JavaScript",
+        complete: true,
+        dueDate: "2019-11-11",
+      },
+      {
+        id: 4,
+        taskName: "Learn React",
+        complete: false,
+        dueDate: "2020-01-06",
+      }
+    ]
+	})
+})
+
+
+//POST
+
+//PUT
+
+//DELETE
+
+module.exports.app = serverlessHttp(app);
